7d2c94ece16759427f523b63d50451a8cd8baeb2
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-02 10:02:57 -0500

    add: pseudo code for js

---
+++ memegen.js
@@ -0,0 +1,10 @@
+// when form submitted
+  // create div with class (?) to hold meme image
+  // create img with class (?)
+  // fetch image from url
+    // -- when img returns, display img within dom image created
+  // attach img inside div
+  // display div img in DOM
+
+
+  // listen for click on image to remove from DOM
