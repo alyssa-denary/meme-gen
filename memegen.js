9a8ec5446e9eb500aa7529839da4051f6f632cb3
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-08 16:00:10 -0500

    add: definitions and update pseudocode

--- memegen.js
+++ memegen.js
@@ -1,11 +1,44 @@
+const urlEl = document.querySelector("#img-url");
+const topTextEl = document.querySelector("#top-text");
+const bottomTextEl = document.querySelector("#bottom-text");
+
+/*
 // when form submitted
+  // disable default refresh
   // extract user submitted url
   // create div with class (?) to hold meme image
   // create img with class (?)
-  // fetch image from url
-    // -- when img returns, display img within dom image created
+  // add img url to img src
   // attach img inside div
+  // display top text over img
+  // display bottom text over img
+  // display div img in DOM
+*/
+
+// listen for click on image to remove from DOM
+
+// when form submitted
+document.querySelector(".meme-form-button").addEventListener("click", (e) => {
+  // disable auto refresh
+  e.preventDefault();
+  // extract user submitted url
+  const url = urlEl.value;
+
+  // render image
+  // reset form values
+});
+
+// listen for click on image to remove from DOM
+
+/*
+// render image function:
+// inputs: src of image, topText, bottomText
+  // create div with class (?) to hold meme image
+  // create img with class (?)
+  // add img url to img src
+  // attach img inside div
+  // display top text
+  // display bottom text
   // display div img in DOM
 
-
-  // listen for click on image to remove from DOM
+*/
