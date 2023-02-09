2e86ba57d779ec09f29c33a4cde2bea49acbfdcf
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-09 16:59:07 -0500

    refactor: reorg code

--- utils.js
+++ utils.js
@@ -1,18 +1,19 @@
 "use strict";
 
 function renderMeme(obj) {
-  // create div to hold meme image
+  // Container creation & styling
   const container = document.createElement("section");
   container.style.margin = "2rem";
   container.style.position = "relative";
   container.style.display = "inline-flex";
-  // style & display img
+  document.querySelector("#display-memes").appendChild(container);
+  // Image creation & styling
   const image = document.createElement("img");
   image.src = obj.url;
   image.style.minWidth = "450px";
   image.style.borderRadius = "5px";
   container.appendChild(image);
-  // style & display top text
+  // Top text creation & styling
   const textTop = document.createElement("p");
   textTop.textContent = obj.topText;
   textTop.style.fontFamily =
@@ -20,14 +21,13 @@ function renderMeme(obj) {
   textTop.style.fontSize = "3rem";
   textTop.style.color = "rgb(250, 250, 250)";
   textTop.style.textShadow = "0px 0px 5px rgb(0, 0, 0)";
-  container.appendChild(textTop);
-  // postiion top text
   textTop.style.textAlign = "center";
   textTop.style.position = "absolute";
   textTop.style.top = "5px";
   textTop.style.left = "0px";
   textTop.style.right = "0px";
-  // style & display bottom text
+  container.appendChild(textTop);
+  // Bottom text creation & styling
   const textBottom = document.createElement("p");
   textBottom.textContent = obj.bottomText;
   textBottom.style.fontFamily =
@@ -35,18 +35,14 @@ function renderMeme(obj) {
   textBottom.style.fontSize = "3rem";
   textBottom.style.color = "rgb(250, 250, 250)";
   textBottom.style.textShadow = "0px 0px rgb(0, 0, 0)";
-  container.appendChild(textBottom);
-  // position bottom text
   textBottom.style.textAlign = "center";
   textBottom.style.position = "absolute";
   textBottom.style.bottom = "5px";
   textBottom.style.left = "0px";
   textBottom.style.right = "0px";
-  // display container in DOM
-  document.querySelector("#display-memes").appendChild(container);
+  container.appendChild(textBottom);
 }
 
-// Function create id
 function createID(usedIds) {
   let min = 1;
   let max = 1000000;
@@ -59,7 +55,6 @@ function createID(usedIds) {
   }
 }
 
-// Create new meme
 function createMeme(usedIds, memes, url, topText, bottomText) {
   const meme = {
     id: createID(usedIds),
@@ -72,12 +67,10 @@ function createMeme(usedIds, memes, url, topText, bottomText) {
   return meme;
 }
 
-// Store in localStorage function
 function store(key, obj) {
   localStorage.setItem(`${key}`, JSON.stringify(obj));
 }
 
-// Retrieve from localStorage function
 function retrieve(key) {
   const objJSON = localStorage.getItem(`${key}`);
   return objJSON !== null ? JSON.parse(objJSON) : [];
